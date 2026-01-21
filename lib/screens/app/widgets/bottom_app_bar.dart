5250641048602dbbb9bf0dbd8392825a44ea9347
Haseeb Khalid <sparksilicon@gmail.com>
2026-01-21 12:30:46 +0500

    UI(app): improve bottom app bar ui

--- lib/screens/app/widgets/bottom_app_bar.dart
+++ lib/screens/app/widgets/bottom_app_bar.dart
@@ -18,100 +18,121 @@ class MyBottomAppBar extends StatelessWidget {
       elevation: 0.0,
       height: FetchPixels.getPixelHeight(70),
       shape: CircularNotchedRectangle(),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: FetchPixels.getPixelWidth(5)),
-        child: Selector<AppProvider, int>(
-          selector: (context, appProvider) => appProvider.currentPageIndex,
-          builder: (context, index, child) => Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                spacing: 20,
-                children: [
-                  Container(
+      child: Selector<AppProvider, int>(
+        selector: (context, appProvider) => appProvider.currentPageIndex,
+        builder: (context, index, child) => Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 15,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (appProvider.currentPageIndex != 0) {
+                      appProvider.changePageIndex(0);
+                    }
+                  },
+                  child: Container(
                     alignment: Alignment.center,
-                    height: FetchPixels.getPixelHeight(50),
-                    width: FetchPixels.getPixelWidth(50),
-                    child: IconButton(
-                      onPressed: () {
-                        if (appProvider.currentPageIndex != 0) {
-                          appProvider.changePageIndex(0);
-                        }
-                      },
-                      icon: SvgPicture.asset(
-                        R.icons.homeIcon,
-                        height: FetchPixels.getPixelHeight(
-                          (index == 0) ? 36 : 23,
-                        ),
+                    height: FetchPixels.getPixelHeight(60),
+                    width: FetchPixels.getPixelWidth(60),
+                    child: SvgPicture.asset(
+                      R.icons.homeIcon,
+                      height: FetchPixels.getPixelHeight(
+                        (index == 0) ? 29 : 23,
+                      ),
+                      colorFilter: ColorFilter.mode(
+                        (index != 0)
+                            ? R.colors.lightGreyColor
+                            : R.colors.whiteColor,
+                        BlendMode.srcIn,
                       ),
                     ),
                   ),
-                  Container(
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (appProvider.currentPageIndex != 1) {
+                      appProvider.changePageIndex(1);
+                    }
+                  },
+                  child: Container(
                     alignment: Alignment.center,
-                    height: FetchPixels.getPixelHeight(50),
-                    width: FetchPixels.getPixelWidth(50),
-                    child: IconButton(
-                      onPressed: () {
-                        if (appProvider.currentPageIndex != 1) {
-                          appProvider.changePageIndex(1);
-                        }
-                      },
-                      icon: SvgPicture.asset(
-                        R.icons.dashboardIcon,
-                        height: FetchPixels.getPixelHeight(
-                          (index == 1) ? 25 : 20,
-                        ),
+                    height: FetchPixels.getPixelHeight(60),
+                    width: FetchPixels.getPixelWidth(60),
+                    child: SvgPicture.asset(
+                      R.icons.dashboardIcon,
+                      height: FetchPixels.getPixelHeight(
+                        (index == 1) ? 25 : 20,
+                      ),
+                      colorFilter: ColorFilter.mode(
+                        (index != 1)
+                            ? R.colors.lightGreyColor
+                            : R.colors.whiteColor,
+                        BlendMode.srcIn,
                       ),
                     ),
                   ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                spacing: 20,
-                children: [
-                  Container(
+                ),
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 15,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (appProvider.currentPageIndex != 2) {
+                      appProvider.changePageIndex(2);
+                    }
+                  },
+                  child: Container(
                     alignment: Alignment.center,
-                    height: FetchPixels.getPixelHeight(50),
-                    width: FetchPixels.getPixelWidth(50),
-                    child: IconButton(
-                      onPressed: () {
-                        if (appProvider.currentPageIndex != 2) {
-                          appProvider.changePageIndex(2);
-                        }
-                      },
-                      icon: SvgPicture.asset(
-                        R.icons.analyticsIcon,
-                        height: FetchPixels.getPixelHeight(
-                          (index == 2) ? 25 : 20,
-                        ),
+                    height: FetchPixels.getPixelHeight(60),
+                    width: FetchPixels.getPixelWidth(60),
+                    child: SvgPicture.asset(
+                      R.icons.analyticsIcon,
+                      height: FetchPixels.getPixelHeight(
+                        (index == 2) ? 25 : 20,
+                      ),
+                      colorFilter: ColorFilter.mode(
+                        (index != 2)
+                            ? R.colors.lightGreyColor
+                            : R.colors.whiteColor,
+                        BlendMode.srcIn,
                       ),
                     ),
                   ),
-                  Container(
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (appProvider.currentPageIndex != 3) {
+                      appProvider.changePageIndex(3);
+                    }
+                  },
+                  child: Container(
                     alignment: Alignment.center,
-                    height: FetchPixels.getPixelHeight(50),
-                    width: FetchPixels.getPixelWidth(50),
-                    child: IconButton(
-                      onPressed: () {
-                        if (appProvider.currentPageIndex != 3) {
-                          appProvider.changePageIndex(3);
-                        }
-                      },
-                      icon: SvgPicture.asset(
-                        R.icons.profileIcon1,
-                        height: FetchPixels.getPixelHeight(
-                          (index == 3) ? 25 : 20,
-                        ),
+                    height: FetchPixels.getPixelHeight(60),
+                    width: FetchPixels.getPixelWidth(60),
+                    child: SvgPicture.asset(
+                      R.icons.profileIcon1,
+                      height: FetchPixels.getPixelHeight(
+                        (index == 3) ? 25 : 20,
+                      ),
+                      colorFilter: ColorFilter.mode(
+                        (index != 3)
+                            ? R.colors.lightGreyColor
+                            : R.colors.whiteColor,
+                        BlendMode.srcIn,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
