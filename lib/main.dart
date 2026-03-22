5fae7efcd51a2feed331ad3ff7be09564194572c
Haseeb Khalid <sparksilicon@gmail.com>
2026-03-22 14:53:24 +0500

    feat: adds the validator file

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:expenzo/screens/auth/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
 void main() async {
   runApp(
     MultiProvider(
